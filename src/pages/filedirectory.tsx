37ed363c88d95d3c9b49bfd2b63c268c0056bbab
Farhan Kamal <132031492+Farhan-Kamal@users.noreply.github.com>
2024-11-08 12:40:19 -0500

    Update filedirectory.tsx

--- src/pages/filedirectory.tsx
+++ src/pages/filedirectory.tsx
@@ -42,9 +42,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #3",
         type: "file" as const,
         content: {
-          title: "CFU #3",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #3 | Mathematical operations ",
+          description: "In the simplest form CFU # 3 was all about making a simple calculator. We overall used our knowledge on math operators and just ask for 2 inputs 1st number and 2nd number these will be used for the calculation.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_0.py",
         },
       },
       {
@@ -52,9 +52,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #4",
         type: "file" as const,
         content: {
-          title: "CFU #4",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #4 | Quadratic Equation Calculator",
+          description: "Think of CFU # 3 as our simplier version of this. Overall we had to use our basic knowledge in math to add the Quadartic equation into Python we go ahead and ask for the Coefficient of the standard form equation then calculate the Roots of the Quadratic equation. hen lastly print it out.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_1.py",
         },
       },
       {
@@ -62,9 +62,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #5",
         type: "file" as const,
         content: {
-          title: "CFU #5",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #5 | Coins to cash converter",
+          description: "We made a program similar to CFU # 3 and 4 where we get numbers and output the result. Here we are asking for a certain amount of coins like pennies, nickles, dimes, and much more. then once we get those we calculate and add up the coins and dived them via dollars to overall get the output which is coins in dollars.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_2.py",
         },
       },
       {
@@ -72,9 +72,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #6",
         type: "file" as const,
         content: {
-          title: "CFU #6",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #6 | Advance use of Mathematical operations",
+          description: "Like in CFU # 3 we are going to show off all our wys of calculations. But also add on by using random which helps generate a random number.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_3.py",
         },
       },
       {
@@ -82,9 +82,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #7",
         type: "file" as const,
         content: {
-          title: "CFU #7",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #7 | The Sum or Average",
+          description: "We are pretty much asking the user for 3 numbers. Once we got them we add them up to get a sum from all of them then we go ahead and use those same numbers to find the average between them.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_4.py",
         },
       },
       {
@@ -92,9 +92,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #8",
         type: "file" as const,
         content: {
-          title: "CFU #8",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #8 | The Grub Hub Copy",
+          description: "A copycat of GrubHub Simplified. we ask if the user order food if yes then yay if not oh well. Then we ask for the cost of the food and also how many were eating or how many are splitting the order.",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_5.py",
         },
       },
       {
@@ -102,9 +102,9 @@ const FILE_TREE_DATA = [
         name: "Cfu #9",
         type: "file" as const,
         content: {
-          title: "CFU #9",
-          description: "This was our first Check for Understanding assigment overall it was simple. All we needed to do was show some basic understanding we have so far of Python and use it for this CFU.",
-          url: "https://py3.codeskulptor.org/#user309_7eooRclogq_0.py",
+          title: "CFU #9 | Roll the Dice Game",
+          description: "We are making a simple python game where we ask how many times a user might want to play then we get the user to guess a number between 1-6 (Like how a dice has 1-6 if they get it right they get 6+ points if they get it wrong they lose 1 point after the rolls they inputed we print their final score.)",
+          url: "https://py3.codeskulptor.org/#user309_iMT9WkUmpB_6.py",
         },
       },
       {
@@ -365,7 +365,7 @@ export default function Home() {
   };
   return (
     <div className="flex h-screen bg-background grid grid-cols-3">
-      <div className="col-span-1 w-64 border-r bg-muted/10">
+      <div className="overflow-y-auto col-span-1 w-64 border-r bg-muted/10">
         <div className="p-2 font-semibold text-sm">Explorer</div>
         <button type="button" onClick={handleGoBack}>
               Return
@@ -375,7 +375,7 @@ export default function Home() {
             onSelect={(node) => node.content && setSelectedContent(node.content)}
           />
       </div>
-      <div className="col-span-2 flex-1 overflow-auto">
+      <div className="overflow-y-auto top-0 col-span-2 flex-1 overflow-auto">
         <ContentView {...selectedContent} />
       </div>
     </div>
